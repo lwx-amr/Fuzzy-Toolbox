6440c855351854d6fcde5a6f07573e660b251de6
amrister <amr.ister20@gmail.com>
2019-12-06 20:57:58 +0200

    Project base is ready

---
+++ src/Fuzzification.java
@@ -0,0 +1,12 @@
+
+public class Fuzzification {
+
+	// Variables
+	
+	
+	// Constructor
+	public Fuzzification () {
+		
+	}
+	
+}
---
+++ src/FuzzyToolBox.java
@@ -0,0 +1,24 @@
+
+public class FuzzyToolBox {
+
+	// Variables
+	
+	
+	// Constructor
+	public FuzzyToolBox () {
+		
+	}
+	
+	// Function to add new fuzzy set ( member function )
+	public void addFuzzySet() {
+		
+	}
+	
+	// Function to fuzzy crisp input
+	public void fuzzifyCrisp() {
+		
+	}
+	
+	//  Function to process given rules and combine their outputs
+	
+}
